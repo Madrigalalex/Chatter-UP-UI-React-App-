f9c707f5d12cd40c36f53ce9b6e64582833215bf
Alex Madrigal <43197543+Madrigalalex@users.noreply.github.com>
2020-05-10 21:26:54 -0700

    uploading files

---
+++ types.js
@@ -0,0 +1,21 @@
+//User reducer types
+export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
+export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
+export const SET_USER = 'SET_USER';
+export const LOADING_USER = 'LOADING_USER';
+export const MARK_NOTIFICATIONS_READ = 'MARK_NOTIFICATIONS_READ';
+
+//UI reducer types
+export const SET_ERRORS = 'SET_ERRORS';
+export const LOADING_UI = 'LOADING_UI';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const LOADING_DATA = 'LOADING_DATA';
+export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+//Data reducer types
+export const SET_CHITCHATS = 'SET_CHITCHATS';
+export const SET_CHITCHAT =  'SET_CHITCHAT';
+export const LIKE_CHITCHAT = 'LIKE_CHITCHAT';
+export const UNLIKE_CHITCHAT = 'UNLIKE_CHITCHAT';
+export const DELETE_CHITCHAT = 'DELETE_CHITCHAT';
+export const POST_CHITCHAT = 'POST_CHITCHAT';
+export const SUBMIT_COMMENT = 'SUBMIT_COMMENT';
